d54feb85d06996a31e6a776ea5df27361896fd9d
Landon Schropp <schroppl@gmail.com>
2026-05-21 23:47:22 -0700

    Wire App to provide the store

--- src/tui/app.tsx
+++ src/tui/app.tsx
@@ -1,14 +1,21 @@
 import { Footer } from "./components/footer.tsx";
 import { Header } from "./components/header.tsx";
 import { SessionList } from "./components/session-list.tsx";
+import { StoreProvider } from "./state/store.tsx";
 import { Box } from "ink";
+
+// TODO: Replace this hardcoded value with a `useNumberOfColumns` hook that derives it from the
+// terminal width.
+const INITIAL_NUMBER_OF_COLUMNS = 3;
 
 export function App() {
   return (
-    <Box flexDirection="column">
-      <Header />
-      <SessionList />
-      <Footer />
-    </Box>
+    <StoreProvider initialNumberOfColumns={INITIAL_NUMBER_OF_COLUMNS}>
+      <Box flexDirection="column">
+        <Header />
+        <SessionList />
+        <Footer />
+      </Box>
+    </StoreProvider>
   );
 }
